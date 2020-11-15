d55158c39d988f21874c3e48aa117f62f120955c
Gytis Daujotas <gytdau@gmail.com>
2020-11-15 03:44:42 +0000

    Second sprint done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import socketIOClient from "socket.io-client";
+import confetti from 'canvas-confetti';
+
+const ENDPOINT = "http://localhost:8000";
+let socket;
 
 class Player extends React.Component {
   render() {
+    let name = ""
+    switch(this.props.orientation) {
+      case "top":
+        name = "Partner"
+        break;
+      default:
+      case "bottom":
+        name = "You";
+        break;
+      case "left":
+        name = "Opponent 1";
+        break;
+      case "right":
+        name = "Opponent 2";
+    }
+    let {player} = this.props;
+    if(player.state == "GONE") {
+      return null
+    }
+    let card = "TS"
+    if(player.card) {
+      card = player.card
+    }
+    if(player.state == "PLAY") {
+      card = "1B"
+    }
+    if(player.state == "WAIT") {
+      card = "empty"
+    }
     return <div className={"player-" + this.props.orientation}>
-      <Card of={this.props.player} />
+      <div className="player">
+      <Card of={card}/>
+      <div className="player-name">
+        {name}
+      </div>
+      </div>
     </div>
   }
 }
 
 class Card extends React.Component {
   render() {
-    return <img className="card" src={"/cards/" + this.props.of + ".svg"} />
+    return <img {...this.props} className="card" src={"/cards/" + this.props.of + ".svg"} />
   }
-}
-
-function bezier(t) {
-  return t * t * (3.0 - 2.0 * t);
 }
 class Cards extends React.Component {
   render() {
+    if(!this.props.cards) {
+      return null
+    }
     return this.props.cards.map((card, index) => {
       let len = this.props.cards.length;
       let proportion = (index + 0.5) / len;
@@ -30,7 +68,7 @@ class Cards extends React.Component {
         {"transform": `rotate(${rotation}deg)`,
       "margin-top": `${margin}px`}
         }>
-        <Card of={card}/>
+        <Card of={card} onClick={() => {this.props.playCard(index)}}/>
       </div>
     })
   }
@@ -39,49 +77,105 @@ class Cards extends React.Component {
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { loading: true, players: ["4H", "4H", "4H", "4H"], 
-    cards: ["4H", "KD", "TC", "AC"], thisPlayer: 0 };
+    this.state = { loading: true };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    socket = socketIOClient(ENDPOINT);
+    socket.on("connect", () => {
+      socket.emit("join game")
+    })
+    socket.on("reconnect please", () => {
+      socket.emit("join game")
+    })
+    socket.on("welcome", (player_id) => {
+      this.setState({player_id})
+    })
+    socket.on("update_data", data => {
+      console.log(data)
+      if(this.state.data && this.state.data.game_state == "STARTED" && data.game_state == "FINISHED" && data.teams[data.players[this.state.player_id].team_id].has_won) {
+        throwConfetti()
+      }
+      this.setState({data, loading: false})
+    });
+  }
+
   render() {
+    if(this.state.loading) {
+      return null
+    }
+    let {data, player_id} = this.state
+    let {players, teams, game_state, playerCount} = data
+    if(players[player_id] == undefined) {
+      return null
+    }
+
+    let thisTeamScore = teams[players[player_id].team_id].score
+    let otherTeamScore = teams[teams[players[player_id].team_id].enemy_team].score
+    let header = null;
+    if(game_state == "WAITING") {
+      header = <div className="header">
+      {playerCount}/4 Players Connected. Share this link with your friends!
+    </div>
+    }
+    if(game_state == "STARTED") {
+      header = <div className="header">
+          Your Team {thisTeamScore} - {otherTeamScore} Opponent Team
+        </div>
+    }
+    if(game_state == "FINISHED") {
+      header = teams[players[player_id].team_id].has_won ? <div className="header header-win">
+          Your team has won!
+        </div> : <div className="header header-lost">
+          You've lost this round :(
+        </div>
+    }
     return (
       <div className="game">
-        <div className="header">
-          Your Team 1-2 Opponent Team
-        </div>
+        {header}
         {this.getPlayers()}
-        <div className="footer">
-          <Cards cards={this.state.cards} />
+        <div className={`footer ${players[player_id].state != "PLAY" ? "footer-withdrawn" : ""}`}>
+          <Cards cards={players[player_id].deck} playCard={this.playCard.bind(this)}/>
         </div>
       </div>
     );
   }
 
-  getPlayers() {
-    let bottom = this.state.thisPlayer;
-    let top = 0;
-    let left = 0;
-    let right = 0;
-    if (bottom == 0) {
-      top = 1;
-      left = 2;
-      right = 3;
-    } else if (bottom == 1) {
-      top = 0;
-      left = 2;
-      right = 3;
-    } else if (bottom == 2) {
-      top = 3;
-      left = 0;
-      right = 1;
-    } else if (bottom = 3) {
-      top = 2;
-      left = 0;
-      right = 1;
+  playCard(card_index) {
+    let {data, player_id} = this.state
+    let {players, teams} = data
+    if(players[player_id].state != "PLAY") {
+      return
     }
-    let { players } = this.state;
+    socket.emit("play card", {card_index, player_id})
+  }
+
+  getPlayers() {
+    let bottom = this.state.player_id;
+    let top = 1;
+    let left = 1;
+    let right = 1;
+    if (bottom == 1) {
+      top = 3;
+      left = 2;
+      right = 4;
+    } else if (bottom == 2) {
+      top = 4;
+      left = 3;
+      right = 1;
+    } else if (bottom == 3) {
+      top = 1;
+      left = 4;
+      right = 2;
+    } else if (bottom = 4) {
+      top = 2;
+      left = 1;
+      right = 3;
+    }
+    let { data } = this.state;
+    let {players} = data;
     top = players[top];
     left = players[left];
     right = players[right];
@@ -117,3 +211,30 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+function throwConfetti() {
+  var end = Date.now() + (5 * 1000);
+  var colors = ['#2ECC40', '#39CCCC', '#0074D9'];
+
+  (function frame() {
+    confetti({
+      particleCount: 2,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 },
+      colors: colors
+    });
+    confetti({
+      particleCount: 2,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 },
+      colors: colors
+    });
+
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  }());
+}
